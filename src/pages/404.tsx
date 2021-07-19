b81acd0adc7c1f07f0345ad4e05c10011de997ab
赤琦 <redblue9771@gmail.com>
2021-07-19 17:37:32 +0800

    fix: remove mainlayout for every page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,10 @@
-import { MainLayout } from "@/features/layouts"
 import React from "react"
 
 const NotFoundPage = () => (
-  <MainLayout>
-    {/* <Seo title="404: Not found" /> */}
-    <div className="container text-center my-5">
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
-  </MainLayout>
+  <div className="container text-center my-5">
+    <h1>404: Not Found</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  </div>
 )
 
 export default NotFoundPage
