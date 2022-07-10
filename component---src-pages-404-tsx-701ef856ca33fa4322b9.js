1c447873d66d860cdd3cc0644b44c3a71253bc45
赤琦 <redblue9771@icloud.com>
2022-07-10 15:49:46 +0800

    Updates
    Former-commit-id: 567bd9a8a89ef50d8c38649f5f8ab3e4a4b2403f
    Former-commit-id: 94d5b883000d748a365ab260f25e052ad404b1ae

--- component---src-pages-404-tsx-701ef856ca33fa4322b9.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunk_r_ichard_gatsby_starter_bootstrap_5=self.webpackChunk_r_ichard_gatsby_starter_bootstrap_5||[]).push([[218],{8908:function(){},938:function(t,e,a){"use strict";a.r(e);var n=a(8908),r=a(7294);e.default=function(){return r.createElement(n.MainLayout,null,r.createElement("div",{className:"container text-center my-5"},r.createElement("h1",null,"404: Not Found"),r.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
-//# sourceMappingURL=component---src-pages-404-tsx-701ef856ca33fa4322b9.js.map
--- component---src-pages-404-tsx-7b89c7a08cc4107b07a4.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunk_r_ichard_gatsby_starter_bootstrap_5=self.webpackChunk_r_ichard_gatsby_starter_bootstrap_5||[]).push([[218],{938:function(t,e,a){"use strict";a.r(e);var r=a(7294);e.default=function(){return r.createElement("div",{className:"container text-center my-5"},r.createElement("h1",null,"404: Not Found"),r.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-tsx-7b89c7a08cc4107b07a4.js.map
